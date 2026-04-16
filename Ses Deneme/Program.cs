61bd4d5bb0d881734f82e684a989305d9bba547a
Cem <cemojryt@gmail.com>
2026-04-16 21:34:36 +0300

    apdeyt

--- Ses Deneme/Program.cs
+++ Ses Deneme/Program.cs
@@ -1,5 +1,6 @@
 ﻿using NAudio.Wave;
 using System.ComponentModel.Design;
+using System.Security.Cryptography;
 
 namespace Ses_Deneme
 {
@@ -9,6 +10,7 @@ namespace Ses_Deneme
         {
             AudioFileReader audio = null;
             float[] buffer = null;
+            float gain;
 
             bool work = true;
             bool loaded = false;
@@ -35,7 +37,7 @@ namespace Ses_Deneme
 
                 Console.WriteLine("Gain        [Press 2]");
                 Console.WriteLine("Reverse     [Press 3]");
-                Console.WriteLine("Save        [Press 4]");
+                Console.WriteLine("Export      [Press 4]");
                 Console.WriteLine("Exit        [Press 0]");
 
                 string choice = Console.ReadKey().KeyChar.ToString();
@@ -57,24 +59,90 @@ namespace Ses_Deneme
                         loaded = true;
 
                         audio = new AudioFileReader(path);
-                        buffer = new float[(int)(audio.Length / 4)];
+                        buffer = new float[(int)(audio.Length / sizeof(float))];
                         audio.Read(buffer, 0, buffer.Length);
                         
                         Console.WriteLine("\r\n" + "File Loaded" + "\r\n");
                         Console.WriteLine("Press Any Key To Return" +"\r\n");
                         Console.ReadKey();
                         Console.Clear();
+
                         break;
 
                     case "2":
+
+                        if (!loaded)
+                        {
+                            break;
+                        }
+
+                        Console.Clear();
+                        Console.Write("Enter The Gain Value: ");
+
+                        bool success = float.TryParse(Console.ReadLine(), out gain);
+
+                        if (success && gain != 0)
+                        {
+                            for (int i = 0; i < buffer.Length; i++)
+                            {
+                                buffer[i] *= gain;
+                            }
+                            Console.WriteLine("\r\n" + "Gain Applied" + "\r\n");
+                        }
+                        else
+                        {
+                            Console.WriteLine("\r\n" + "Invalid Gain Value" + "\r\n");
+                        }
+
+                        Console.WriteLine("Press Any Key To Return" + "\r\n");
+                        Console.ReadKey();
+                        Console.Clear();
 
                         break;
 
                     case "3":
 
+                        if (!loaded)
+                        {
+                            break;
+                        }
+
+                        Console.Clear();
+                        Array.Reverse(buffer);
+
+                        Console.WriteLine("Audio Reversed" + "\r\n");
+                        Console.WriteLine("Press Any Key To Return" + "\r\n");
+                        Console.ReadKey();
+                        Console.Clear();
+
                         break;
 
                     case "4":
+
+                        if (!loaded)
+                        {
+                            break;
+                        }
+
+                        Console.Clear();
+                        Console.Write("Enter Output Path: ");
+
+                        try
+                        {
+                            using (var write = new WaveFileWriter(Console.ReadLine(), audio.WaveFormat))
+                            {
+                                write.WriteSamples(buffer, 0, buffer.Length);
+                            }
+                            Console.WriteLine("\r\n" + "File Exported" + "\r\n");
+                        }
+
+                        catch (Exception)
+                        {
+                            Console.WriteLine("\r\n" + "Invalid Output Path" + "\r\n");
+                        }
+                        Console.WriteLine("Press Any Key To Return" + "\r\n");
+                        Console.ReadKey();
+                        Console.Clear();
 
                         break;
 
@@ -84,6 +152,7 @@ namespace Ses_Deneme
                 }
             }
             Console.ReadLine();
+            //C:\Users\ozlem\Desktop\FL Studio Musics\This Is Awesome.mp3
         }
     }
 }
